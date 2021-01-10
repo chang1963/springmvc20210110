2e8b71202fa98e4a34b0eaaf4b26dab54788da4b
chang1963 <tingchenchang1963@outlook.com>
2021-01-10 16:29:59 +0800

    First Spring XML MVC

---
+++ spring20210110/src/main/java/demo/springmvc/PathVariableController.java
@@ -0,0 +1,40 @@
+package demo.springmvc;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+ 
+@Controller
+public class PathVariableController {   
+    /**
+     * @PathVariable Example:
+     *
+     */    @RequestMapping("/web/fe/{sitePrefix}/{language}/document/{id}/{naturalText}")
+    public String documentView(Model model,
+            @PathVariable(value = "sitePrefix") String sitePrefix,
+            @PathVariable(value = "language") String language,
+            @PathVariable(value = "id") Long id,
+            @PathVariable(value = "naturalText") String naturalText) {
+ 
+        model.addAttribute("sitePrefix", sitePrefix);
+        model.addAttribute("language", language);
+        model.addAttribute("id", id);
+        model.addAttribute("naturalText", naturalText);
+ 
+        String documentName = "Java tutorial for Beginners";
+        if(id == 8080) {
+            documentName = "Spring MVC for Beginners";
+        }
+ 
+        model.addAttribute("documentName", documentName);
+ 
+        return "documentView";
+    }
+     @RequestMapping(value = "/save")
+     @ResponseBody
+     public String authorInfo(Model model) {
+         return "saved";
+     } 
+
+}
